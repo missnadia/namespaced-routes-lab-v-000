a8ad187f0588110090b82044fcad7196e3a24e34
Nadia Lee <miss.nadia.lee@gmail.com>
2018-06-18 23:39:34 +0000

    automatically backed up by learn

--- app/controllers/admin/preferences_controller.rb
+++ app/controllers/admin/preferences_controller.rb
@@ -1,10 +1,12 @@
 class Admin::PreferencesController < ApplicationController
   def index
-    @preference = Preference.create(
+    @preferences = Preference.create(
       artist_sort_order: "DESC",
       song_sort_order: "ASC",
       allow_create_artists: false,
       allow_create_songs: true
     )
   end
+
+
 end
