0d83ae4c12c7827a420fdfbad3818e3c4ca0e1d7
Nadia Lee <miss.nadia.lee@gmail.com>
2018-06-18 23:52:14 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,5 +1,6 @@
 class ArtistsController < ApplicationController
   before_action :set_preferences, only: [:index, :new]
+
   def index
     if @preferences && @preferences.artist_sort_order
       @artists = Artist.order(name: @preferences.artist_sort_order)
